db455db28c9e2ae486abe9311d6a9b69117c8f42
AkitoYamashita <akt.ymst@gmail.com>
2023-12-28 00:49:22 +0900

    fast commit

--- docs/assets/index.html-3ssjKk3l.js
+++
@@ -1,1 +0,0 @@
-const t=JSON.parse('{"key":"v-8daa1a0e","path":"/","title":"README","lang":"ja","frontmatter":{},"headers":[],"git":{"updatedTime":null,"contributors":[]},"filePathRelative":"README.md","excerpt":""}');export{t as data};
---
+++ docs/assets/index.html-KTu8cwO7.js
@@ -0,0 +1,1 @@
+const a=JSON.parse('{"key":"v-8daa1a0e","path":"/","title":"README","lang":"ja","frontmatter":{},"headers":[],"git":{"updatedTime":1703691382000,"contributors":[{"name":"AkitoYamashita","email":"akt.ymst@gmail.com","commits":1}]},"filePathRelative":"README.md","excerpt":""}');export{a as data};
