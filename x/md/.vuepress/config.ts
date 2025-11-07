5091726a7b3637ff6e950ab0d984a2ef88951946
AkitoYamashita <akt.ymst@gmail.com>
2025-11-08 01:26:01 +0900

    fix10

--- x/md/.vuepress/config.ts
+++ x/md/.vuepress/config.ts
@@ -59,14 +59,12 @@ export default {
     // TailwindCSS
     ['script', {},`
       tailwind.config = {
-        corePlugins: { preflight: false },
-        plugins: [tailwindTypography],
         theme: {
           extend: {}
         }
       }
     `],
-    ['script', { src: 'https://cdn.tailwindcss.com?plugins=typography' }],
+    ['script', { src: 'https://cdn.tailwindcss.com' }],
     // GA
     ['script', { async: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-27XRJCNG7Y' }],
     ['script', {}, ["window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-27XRJCNG7Y');"]],
