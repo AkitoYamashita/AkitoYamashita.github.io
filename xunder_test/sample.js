75080fbe4b1a7601f27295862ada4f968b9e8fa5
AkitoYamashita <akt.ymst@gmail.com>
2026-01-10 19:58:23 +0900

    for xunder file

---
+++ xunder_test/sample.js
@@ -0,0 +1,7 @@
+var test = "x_text_x";
+
+//= begin demo
+var test2 = "x_text_x";
+const x_token_x = 456;
+const api = "[[api_key]]";
+//= end demo
---
+++ xunder_test/sample.md
@@ -0,0 +1,9 @@
+# Sample
+
+## テスト
+
+文字列: [[[DEFAULT]]]
+
+<!--= begin demo -->
+API KEY = [[[api_key]]]
+<!--= end demo -->
